e41be9e41c7f9f5f41f31bb3f3752cab250bb844
diel21 <adielferreira@outlook.com>
2021-11-15 15:18:04 -0300

    Att lista 7

--- src/Lista7/main1.java
+++ src/Lista7/main1.java
@@ -6,12 +6,11 @@ import javax.swing.JOptionPane;
 public class main1 {
     public static void main(String[] args) {
         ArrayList<Funcionario> funcionarios = new ArrayList<>();
-        ArrayList<FuncionarioHorista> funcsHorista = new ArrayList<>();
-        ArrayList<FuncionarioMensalista> funcsMensalista = new ArrayList<>();
         int menu = Integer.parseInt(JOptionPane.showInputDialog("Oque deseja fazer:\n1- Inserir Funcionario\n2- Exibir Funcionarios\n0- Sair"));
+        int escolha;
         while (menu != 0){
             if(menu == 1){
-                int escolha = Integer.parseInt(JOptionPane.showInputDialog("Qual Tipo de Funcionario deseja criar\n1- Funcionario\n2- Funcionario Horista\n3- Funcionario Mensalista"));
+                escolha = Integer.parseInt(JOptionPane.showInputDialog("Qual Tipo de Funcionario deseja criar\n1- Funcionario\n2- Funcionario Horista\n3- Funcionario Mensalista"));
                 if(escolha == 1){
                     Funcionario f1 = new Funcionario();
                     f1.setNome(JOptionPane.showInputDialog("Nome"));
@@ -29,7 +28,7 @@ public class main1 {
                     f1.setNumeroCracha(Integer.parseInt(JOptionPane.showInputDialog("Numero Cracha")));
                     f1.setQtdeHoras(Integer.parseInt(JOptionPane.showInputDialog("Quantidade de Horas")));
                     f1.setValorHora(Double.parseDouble(JOptionPane.showInputDialog("Valor da Hora")));
-                    funcsHorista.add(f1);
+                    funcionarios.add(f1);
                     menu = Integer.parseInt(JOptionPane.showInputDialog("Oque deseja fazer:\n1- Inserir Funcionario\n2- Exibir Funcionarios\n0- Sair"));
                 }
                 if (escolha == 3){
@@ -39,52 +38,27 @@ public class main1 {
                     f1.setSetor(JOptionPane.showInputDialog("Setor"));
                     f1.setNumeroCracha(Integer.parseInt(JOptionPane.showInputDialog("Numero Cracha:")));
                     f1.setSalario(Double.parseDouble(JOptionPane.showInputDialog("Valor do Salario")));
-                    funcsMensalista.add(f1);
+                    funcionarios.add(f1);
                     menu = Integer.parseInt(JOptionPane.showInputDialog("Oque deseja fazer:\n1- Inserir Funcionario\n2- Exibir Funcionarios\n0- Sair"));
                 }
                 else {
                     menu = Integer.parseInt(JOptionPane.showInputDialog("Oque deseja fazer:\n1- Inserir Funcionario\n2- Exibir Funcionarios\n0- Sair"));
                 }
             }
-            if (menu == 2){  
-                int escolha = Integer.parseInt(JOptionPane.showInputDialog("Qual Tipo de Funcionario deseja Exibir\n1- Funcionario\n2- Funcionario Horista\n3- Funcionario Mensalista"));
-                if(escolha == 1){
-                    String todos = "";
-                    for (Funcionario f :funcionarios){
+            if(menu == 2){
+                String todos = "";
+                for (Funcionario f :funcionarios){
                     todos += "Nome: " + f.getNome() +" - "+"Setor: "+f.getSetor()+" - "+"Função: "+f.getFuncao()+" - "+"Numero Cracha: "+f.getNumeroCracha();
-                    }
-                    JOptionPane.showMessageDialog(null, todos);
-                    menu = Integer.parseInt(JOptionPane.showInputDialog("Oque deseja fazer:\n1- Inserir Funcionario\n2- Exibir Funcionarios\n0- Sair"));
                 }
-                if (escolha == 2){
-                    FuncionarioHorista f1 = new FuncionarioHorista();
-                    String todos = "";
-                    for (FuncionarioHorista f :funcsHorista){
-                    todos += "Nome: " + f.getNome() +" - "+"Setor: "+f.getSetor()+" - "+"Função: "+f.getFuncao()+" - "+"Numero Cracha: "+f.getNumeroCracha()
-                            +" - "+"Quantidade de Horas: "+f.getQtdeHoras()+" - "+"Valor da hora: "+f.getValorHora();
-                    }
-                    JOptionPane.showMessageDialog(null,todos);
-                    menu = Integer.parseInt(JOptionPane.showInputDialog("Oque deseja fazer:\n1- Inserir Funcionario\n2- Exibir Funcionarios\n0- Sair"));
-                }
-                if (escolha == 3){
-                    FuncionarioMensalista f1 = new FuncionarioMensalista();
-                    String todos = "";
-                    for (FuncionarioMensalista f :funcsMensalista){
-                    todos += "Nome: " + f.getNome() +" - "+"Setor: "+f.getSetor()+" - "+"Função: "+f.getFuncao()+" - "+"Numero Cracha: "+f.getNumeroCracha()
-                            +" - "+"Salario: "+f.getSalario();
-                    }
-                    JOptionPane.showMessageDialog(null,todos);
-                    menu = Integer.parseInt(JOptionPane.showInputDialog("Oque deseja fazer:\n1- Inserir Funcionario\n2- Exibir Funcionarios\n0- Sair"));
-                }
-                else {
-                    menu = Integer.parseInt(JOptionPane.showInputDialog("Oque deseja fazer:\n1- Inserir Funcionario\n2- Exibir Funcionarios\n0- Sair"));
-                }
+                JOptionPane.showMessageDialog(null, todos);
+                menu = Integer.parseInt(JOptionPane.showInputDialog("Oque deseja fazer:\n1- Inserir Funcionario\n2- Exibir Funcionarios\n0- Sair"));
             }
+            else{
+                menu = Integer.parseInt(JOptionPane.showInputDialog("Oque deseja fazer:\n1- Inserir Funcionario\n2- Exibir Funcionarios\n0- Sair"));
+                }
+        }
             if (menu == 0){
                 System.exit(0);
             }
-            
-        }
-
     }
 }
